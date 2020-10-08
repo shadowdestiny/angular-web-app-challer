4211686824e3ffceb9f4fc710990d5366b5e9213
Luis Marin <lrm.programador@gmail.com>
2020-10-08 16:25:25 -0400

    [CHAL-701]  se ajusta tamaño de la pantalla

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -1,7 +1,7 @@
 import {AfterViewInit, Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from '@angular/core';
 import {Router} from '@angular/router';
-import {StoreService} from "../../service/store.service";
-import {ResizeConstants} from "../../store/constants/resize.constants";
+import {StoreService} from '../../service/store.service';
+import {ResizeConstants} from '../../store/constants/resize.constants';
 
 @Component({
   selector: 'app-header',
@@ -12,6 +12,7 @@ export class HeaderComponent implements OnInit, AfterViewInit {
 
   isOpen = false;
   isMobile = false;
+  isFirst = true;
   @Output() urlRouter = new EventEmitter<string>();
   @Input() isShowMobile = false;
 
@@ -22,20 +23,20 @@ export class HeaderComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit(): void {
-    this.init()
+    this.init();
   }
 
-  init(){
+  init() {
     this.store.getResizeStore().subscribe((data: any) => {
-      if (data.status === ResizeConstants.START){
+      if (data.status === ResizeConstants.START) {
         this.isMobile = data.resize.isMobile;
       }
     });
   }
 
-  pushUrlRouter(){
+  pushUrlRouter() {
     // setTimeout(() => {
-      this.urlRouter.emit(this.router.url);
+    this.urlRouter.emit(this.router.url);
     // }, 1000)
   }
 
@@ -52,38 +53,53 @@ export class HeaderComponent implements OnInit, AfterViewInit {
     this.isOpen = false;
     this.router.navigate(['/about-us']);
   }
+
   onContact() {
     this.isOpen = false;
     this.router.navigate(['/contact']);
   }
+
   onVision() {
     this.isOpen = false;
     this.router.navigate(['/vision']);
   }
+
   onMission() {
     this.isOpen = false;
     this.router.navigate(['/mision']);
   }
+
   onViewChaller() {
     this.isOpen = false;
     this.router.navigate(['/home-challer']);
   }
+
   onPolicy() {
     this.isOpen = false;
     this.router.navigate(['/policy']);
   }
-  onBusiness(){
+
+  onBusiness() {
     this.isOpen = false;
     this.router.navigate(['/business']);
   }
 
-  onStory(){
+  onStory() {
     this.isOpen = false;
     this.router.navigate(['/story']);
   }
 
   ngAfterViewInit(): void {
     this.pushUrlRouter();
+  }
+
+  closed() {
+    if (!this.isFirst && this.isOpen) {
+      this.isOpen = false;
+      this.isFirst = true;
+    } else {
+      this.isFirst = false;
+    }
   }
 
 }
