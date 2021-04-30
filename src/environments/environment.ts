7e21c913c0503fe848223b4802cada5970e49cff
Luis Marin <lrm.programador@gmail.com>
2021-04-30 07:54:41 -0400

    [CHAL-1319][fix] implementacion funcional

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+
 const AUTH_SERVICES_BASE_URL = 'https://apidev.challer.app';
 export const environment = {
   production: false,
